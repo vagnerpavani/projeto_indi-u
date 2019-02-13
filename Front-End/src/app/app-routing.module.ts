3cb601e04baf9f7907bdff9f8e888754e3cb9fb7
tatsutanka <tanaka012@outlook.com>
2019-02-12 22:14:23 -0200

    +cadastro

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 const routes: Routes = [
-  { path: 'login',component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent},
+  { path: '', redirectTo:'/cadastro', pathMatch: 'full'}
 ];
 
 @NgModule({
