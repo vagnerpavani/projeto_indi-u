e0e3ff6758dd11482711ab10ab4173b2c537162a
vagnerpavani <vagner.alves.pavani@gmail.com>
2019-02-15 22:38:43 -0200

    + passport basico de user(register,login,logout,ver dados)

---
+++ Back-End/app/Http/Controllers/API/PassportController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use Auth;
+use App\User;
+use DB;
+
+class PassportController extends Controller
+{
+    public $successStatus = 200;
+
+    public function login(){
+        if (Auth::attempt(['email' => request('email'), 'password' =>
+        request('password')])){
+        
+        $user = Auth::user();
+        $success['token'] = $user -> createToken('MyApp') -> accessToken;
+        return response() -> json(['success' => $success], $this ->
+        successStatus);
+        }
+        else {
+        return response() -> json (['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    public function register(UserRequest $request) {
+        
+        $newUser = new User;
+        $newUser->name = $request->name;
+        $newUser->username = $request->username;
+        $newUser->email = $request->email;
+        $newUser->password = bcrypt($request->password);
+        $newUser->language = $request->language;
+        $newUser->save();
+        $success['token'] = $newUser->createToken('MyApp')->accessToken;
+        $success['name'] = $newUser->name;
+        return response() -> json(['success' => $success], $this ->
+        successStatus);
+    }
+
+    public function getDetails() {
+        $user = Auth::user();
+        return response() -> json(['success' => $user], $this ->
+        successStatus); 
+    }
+
+    public function logout(){
+        $accessToken = Auth::user()->token();
+        DB::table('oauth_refresh_tokens')->where('access_token_id',
+        $accessToken->id)->update(['revoked' => true]);
+        $accessToken->revoke();
+        return response()->json( null, 204);
+    }
+
+}
