c03ca3e70a38f7044a60e2fa5f3a5d3558ca0092
Gabriel Arrais <arrais.gbr@dcc.ufrj.br>
2019-02-17 14:55:15 -0300

    + notification for edit and delete user

---
+++ Back-End/app/Notifications/Delete.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class Delete extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+      $user = $notifiable;
+      return (new MailMessage)
+                  ->greeting("Olá, $user->name!")
+                  ->line('Sua conta foi excluída com sucesso.')
+                  ->line('É uma pena perder um cliente como o senhor.')
+                  ->line('Caso algum dia volte ao nosso portal, será muito bem vindo!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
