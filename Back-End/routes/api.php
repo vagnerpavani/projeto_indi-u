2b442d31d6729417d48bbc9eeb4cf49de4211abc
vagnerpavani <vagner.alves.pavani@gmail.com>
2019-02-17 21:04:52 -0300

    + middleware para checar se usuario eh admin e dar os devidos privilegios

--- Back-End/routes/api.php
+++ Back-End/routes/api.php
@@ -15,23 +15,30 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
 });
 
 //                CRUDs
-
-Route::apiResource('user','UserController');
-Route::apiResource('project', 'ProjectController');
-Route::apiResource('work', 'WorkController');
-Route::apiResource('avaliation', 'AvaliationController');
 
 //------------------------------------------------//
 
 Route::post('login', 'API\PassportController@login');
 Route::post('register', 'API\PassportController@register');
 
+
+
+
 Route::group(['middleware' => 'auth:api'], function() {
     Route::put('self-update' , 'API\PassportController@selfUpdate');
     Route::delete('self-delete','API\PassportController@selfDelete');
     Route::get('logout', 'API\PassportController@logout');
     Route::post('get-details', 'API\PassportController@getDetails');
+
+    Route::group(['middleware' => 'admin',], function ($router) {
+
+        Route::apiResource('project', 'ProjectController');
+        Route::apiResource('work', 'WorkController');
+        Route::apiResource('avaliation', 'AvaliationController');
+        Route::apiResource('user','UserController');
+    });
 });
