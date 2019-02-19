fabbac8001d8f54d7cfe7f6480c8936b8169c911
Ana Luiza <nana.mattoscs@gmail.com>
2019-02-19 15:14:06 -0300

    *alteraçao nas páginas e adiçao de services

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -4,30 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-
-import { PagamentoComponent } from './pages/pagamento/pagamento.component';
 
 import { FormsModule } from '@angular/forms';
 
-import { RelacoesComponent } from './pages/relacoes/relacoes.component';
-
-import { PesquisaComponent } from './pages/pesquisa/pesquisa.component';
+import { HttpClientModule} from '@angular/common/http';
+import { PesquisaService } from './service/pesquisa.service';
+import { LoginService } from './service/login.service';
+import { PagamentoService } from './service/pagamento.service';
 
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { HttpClientModule} from '@angular/common/http';
-import { LoginService } from './service/login.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PagamentoComponent } from './pages/pagamento/pagamento.component';
+import { RelacoesComponent } from './pages/relacoes/relacoes.component';
+import { PesquisaComponent } from './pages/pesquisa/pesquisa.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
-
 import { SetingsPerfilComponent } from './pages/setings-perfil/setings-perfil.component';
 import { AvaliarComponent } from './pages/avaliar/avaliar.component';
 import { PerfilService } from './service/perfil.service';
 import { CriarProjetoComponent } from './pages/criar-projeto/criar-projeto.component';
 import { DetalhesProjetoComponent } from './pages/detalhes-projeto/detalhes-projeto.component';
 import { AlterarProjetosComponent } from './pages/alterar-projetos/alterar-projetos.component';
+
+
 
 @NgModule({
   declarations: [
